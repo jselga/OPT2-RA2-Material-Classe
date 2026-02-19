589f635634e78f7a53ce4593dcbd698fb63dc8f1
jselga <jordi.selga@iescarlesvallbona.cat>
2026-02-19 22:23:36 +0100

    Tipus i fetch bàsic

--- Bloc2/react-notes-app/src/App.tsx
+++ Bloc2/react-notes-app/src/App.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from "react";
 
+const baseUrl = "http://localhost:3001/api/notes";
+// Definim el tipus Note ha de coincidir amb el que ens retorna l'API
+/*{
+    "_id": "6996ff0293fba81414283404",
+    "content": "Una nota",
+    "date": "2026-02-19T12:16:02.224Z", //date es retorna com a string
+    "important": true,
+}*/
+type Note = {
+  _id: string;
+  content: string;
+  date: string;
+  important: boolean;
+};
 
 function App() {
-  
+  const [notes, setNotes] = useState<Note[]>([]);
+  useEffect(() => {
+      fetch(`${baseUrl}`)
+      .then( (res)=>res.json())
+      .then((notes)=> setNotes(notes))
+  }, []);
 
   return (
-    <>
-     <h1>React TS App</h1>
-    </>
-  )
+    <div>
+      <h1>Notes App</h1>
+      <div className="card">
+        <ul>
+      {notes?.map((note)=>(
+        <li key={note._id}>
+          <p>{note.content}</p>
+          <p>created at: {note.date}</p>
+        </li>
+      ))}  
+      </ul>
+      </div>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
