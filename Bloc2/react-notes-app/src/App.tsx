a498f1db631af9fefa6f8fffc8c71d985da81e1a
jselga <jordi.selga@iescarlesvallbona.cat>
2026-02-24 22:20:08 +0100

    frontend GET by ID amb errors

--- Bloc2/react-notes-app/src/App.tsx
+++ Bloc2/react-notes-app/src/App.tsx
@@ -2,26 +2,40 @@ import { useEffect, useState } from "react";
 import type { Note } from "./types/Note";
 import { getAll, getById } from "./services/notes";
 const baseUrl = "http://localhost:3001/api/notes";
-
+const hardcodedId = "69961ea7f15b737c27dcb2a7";
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
-  const [note,setNote] =useState<Note>();
-  
+  const [note, setNote] = useState<Note | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
-    try {
-      // getAll(baseUrl).then((notes)=>setNotes(notes));  
-      getAll(baseUrl).then(setNotes); 
-      getById(baseUrl,'69961ea7f15b737c27dcb2a8').then(setNote);
-    } catch (error) {
-      console.error(error);
-    }
+ 
+    getAll(baseUrl).then(setNotes).catch(console.error);
+
+    getById(baseUrl, hardcodedId)
+      .then(setNote)
+      .catch((error) => {
+        if (error.response?.status === 404) {
+          setError("Nota no trobada");
+        } else if (error.response?.status === 400) {
+          setError("Id mal formatada");
+        } else {
+          setError("Error inesperat");
+        }
+      });
   }, []);
 
   return (
     <div>
       <h1>Notes App</h1>
       <div className="card">
-        <p><strong>Id:</strong> {note?._id} - <strong>Content: </strong> {note?.content}</p>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {note && (
+          <p>
+            <strong>Id:</strong> {note?._id} - <strong>Content: </strong>{" "}
+            {note?.content}
+          </p>
+        )}
         <ul>
           {notes?.map((note) => (
             <li key={note._id}>
