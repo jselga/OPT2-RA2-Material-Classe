1c6705081f0815ed28666ea9ebf0790a69ea6815
jselga <jordi.selga@iescarlesvallbona.cat>
2026-02-24 23:10:40 +0100

    frontend GET by ID millores en el render

--- Bloc2/react-notes-app/src/App.tsx
+++ Bloc2/react-notes-app/src/App.tsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from "react";
 import type { Note } from "./types/Note";
 import { getAll, getById } from "./services/notes";
 const baseUrl = "http://localhost:3001/api/notes";
-const hardcodedId = "69961ea7f15b737c27dcb2a7";
+
+
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
   const [note, setNote] = useState<Note | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null)
   const handleGetById = (id: string) => {
     setError(null);
     setNote(null);
     getById(baseUrl, id)
-      .then(setNote)
+      .then(setSelectedNote)
       .catch((error) => {
         if (error.response?.status === 404) {
           setError("Nota no trobada");
@@ -32,17 +34,20 @@ function App() {
       <h1>Notes App</h1>
       <div className="card">
         {error && <p style={{ color: "red" }}>{error}</p>}
-        {note && (
-          <p style={note?.important?{fontWeight:'800'}:{fontWeight:'200'}}>
-            <strong>Id:</strong> {note?._id} - <strong>Content: </strong>{" "}
-            {note?.content} <strong>Created at: {note?.date}</strong>
-          </p>
-        )}
         <ul>
           {notes?.map((note) => (
             <li key={note._id}>
               <p>{note.content}</p>
-              <button onClick={() => handleGetById(note._id)}>Detalls</button>
+              <button onClick={() => handleGetById(note._id)}>Detalls
+
+              </button>
+              {selectedNote?._id === note._id && (
+                <div>
+                  <p>ID: {selectedNote._id}</p>
+                  <p>Created at: {selectedNote.date}</p>
+                  {note?.important && ( <p><strong>IMPORTANT</strong></p> )}
+                </div>
+              )}
             </li>
           ))}
         </ul>
